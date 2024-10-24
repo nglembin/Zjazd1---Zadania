0c15bb46c0f128198a4802f0fb5e4d806605884f
Nikodem (nyyn) <nikodemglembin@gmail.com>
2024-10-24 20:33:16 +0000

    Update 1_3.cpp

--- 1_3.cpp
+++ 1_3.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 
 int main() {
-int x;
-cout << "Podaj liczbe calkowita: ";
-cin >> x;
+    int x;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> x;
 
-if (x % 2 == 1) {
-    cout << "Liczba jest nieparzysta";
-}
-else if (x==0) {
-    cout << "Liczba jest równa 0";
-}
-else {
-    cout << "Liczba jest parzysta";
-}
+    if (x == 0) {
+        cout << "Liczba jest równa 0";
+    }
+    else if (x % 2 == 0) {
+        cout << "Liczba jest parzysta";
+    }
+    else {
+        cout << "Liczba jest nieparzysta";
+    }
+    
     return 0;
 }
-
-
